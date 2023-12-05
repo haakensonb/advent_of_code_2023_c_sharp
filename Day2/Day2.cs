047db0b0f75f303dd2b2ae898c63b6e99bab4924
haakensonb <untamedhawk44@gmail.com>
2023-12-04 19:32:30 -0700

    Solve Day2, part 1

---
+++ Day2/Day2.cs
@@ -0,0 +1,90 @@
+﻿namespace AdventOfCode2023;
+
+public record GameState
+{
+    public int id;
+
+    public List<Dictionary<string, int>> colorTotals = new List<Dictionary<string, int>>();
+
+    public GameState(string line)
+    {
+        var split = line.Split(": ");
+        this.id = Convert.ToInt32(split[0].Split(" ")[1]);
+        var bagValues = split[1];
+        string[] bags = bagValues.Split(";");
+        foreach (var bag in bags)
+        {
+            var items = bag.Trim().Split(", ");
+            var colorTotal = new Dictionary<string, int>();
+            foreach (var item in items)
+            {
+                var pair = item.Split(" ");
+                var key = pair[1];
+                var val = Convert.ToInt32(pair[0]);
+                colorTotal[key] = val;
+            }
+            this.colorTotals.Add(colorTotal);
+        }
+    }
+
+    public bool IsPossible(Dictionary<string, int> maxPossibleColorTotals)
+    {
+        foreach (var colorTotal in this.colorTotals)
+        {
+            foreach (var maxColor in maxPossibleColorTotals)
+            {
+                if (colorTotal.ContainsKey(maxColor.Key))
+                {
+                    if (colorTotal[maxColor.Key] > maxColor.Value)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
+
+class Day2
+{
+    private static Dictionary<string, int> maxPossibleColorTotals = new Dictionary<string, int>{
+            {"red", 12},
+            {"green", 13},
+            {"blue", 14}
+        };
+    public static string SolvePart1(string input)
+    {
+
+        var lines = input.Split(Environment.NewLine);
+        var validGameIds = lines.Select(line =>
+        {
+            var gameState = new GameState(line);
+            if (gameState.IsPossible(maxPossibleColorTotals))
+            {
+                return gameState.id;
+            }
+            else
+            {
+                return 0;
+            }
+        });
+        return validGameIds.Sum().ToString();
+    }
+
+    public static void Main()
+    {
+        // var line = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        // var line = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red";
+        // var gameState = new GameState(line);
+        // var maxPossibleColorTotals = new Dictionary<string, int>{
+        //     {"red", 12},
+        //     {"green", 13},
+        //     {"blue", 14}
+        // };
+        // Console.WriteLine(gameState.IsPossible(maxPossibleColorTotals));
+        var input = File.ReadAllText("./input.txt");
+        var answerPart1 = Day2.SolvePart1(input);
+        Console.WriteLine($"Day 2 part 1: {answerPart1}");
+    }
+}
---
+++ Day2/Day2UnitTest.cs
@@ -0,0 +1,18 @@
+using AdventOfCode2023;
+
+public class Day2UnitTest
+{
+
+    private static readonly string input1 = @"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    [Fact]
+    public void TestSolvePart1()
+    {
+        var answer = Day2.SolvePart1(input1);
+        Assert.Equal("8", answer);
+    }
+}
