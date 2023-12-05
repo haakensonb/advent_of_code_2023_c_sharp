c7bf1b2d9014d0a434668b1531c0043c61316acc
haakensonb <untamedhawk44@gmail.com>
2023-12-04 21:49:50 -0700

    Setup files for Day3

---
+++ Day3/Day3.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode2023;
+
+class Day3
+{
+    public static string SolvePart1(string input)
+    {
+        return "";
+    }
+
+    public static string SolvePart2(string input)
+    {
+        return "";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Hello");
+    }
+}
---
+++ Day3/Day3UnitTest.cs
@@ -0,0 +1,13 @@
+using AdventOfCode2023;
+
+public class Day3UnitTest
+{
+    private static readonly string input = "";
+
+    [Fact]
+    public void TestSolvePart1()
+    {
+        var answer = Day3.SolvePart1(input);
+        Assert.Equal("", answer);
+    }
+}
