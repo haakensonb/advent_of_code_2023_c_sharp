10f81ad917c9d77253f29dbc2ffe0b62aaef42c6
haakensonb <untamedhawk44@gmail.com>
2023-12-05 17:56:59 -0700

    Setup Day4

---
+++ Day4/Day4.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2023;
+
+class Day4
+{
+    public static string SolvePart1(string input)
+    {
+        return "";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Hello");
+    }
+}
